f0bebebb0844f55c5e8409230e7d659e66df6731
Alex Blair <bclan11@gmail.com>
2021-11-05 15:36:27 -0700

    style and tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,58 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { add, subtract, multiply, divide } from '../math-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+
+test('2 plus 3 should return 5', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 5;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(2, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('4 minus 1 should return 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 3;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(4, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('2 multiply 3 should return 6', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 6;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(2, 3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('6 divide 2 should return 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 3;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divide(6, 2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
