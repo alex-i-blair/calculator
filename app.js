b0138721afceb4f9f8916669d7976e5f7b7a182d
Alex Blair <bclan11@gmail.com>
2021-11-05 16:56:24 -0700

    refactored

--- app.js
+++ app.js
@@ -11,47 +11,21 @@ import { add, subtract, multiply, divide } from './math-utils.js';
   // get second input from the DOM
   // grab the button
   // grab the empty div to inject result into
-const input1 = document.getElementById('input-1');
-const input2 = document.getElementById('input-2');
-const clickAdd = document.getElementById('click-add');
-const clickSubtract = document.getElementById('click-subtract');
-const clickMultiply = document.getElementById('click-multiply');
-const clickDivide = document.getElementById('click-divide');
-const displayResult = document.getElementById('display-result');
 
-// add an event listener to the button that we grabbed from the DOM
-clickAdd.addEventListener('click', () => {
-    const num1 = +input1.value;
-    const num2 = +input2.value;
-    const result = add(num1, num2);
-    displayResult.textContent = result;
-});
-
-clickSubtract.addEventListener('click', () => {
-    const num1 = +input1.value;
-    const num2 = +input2.value;
-    const result = subtract(num1, num2);
-    displayResult.textContent = result;
-});
-
-clickMultiply.addEventListener('click', () => {
-    const num1 = +input1.value;
-    const num2 = +input2.value;
-    const result = multiply(num1, num2);
-    displayResult.textContent = result;
-});
-
-clickDivide.addEventListener('click', () => {
-    const num1 = +input1.value;
-    const num2 = +input2.value;
-    const result = divide(num1, num2);
-    displayResult.textContent = result;
-});
-
-
-
-  // use variable assignment with "+" to make sure they are treated as numbers
-
-  // assign to result variable the output of the math function that we need to import from math-utils.js
-
-  // the change textContent of the result div
+function setupOperation(id, operation) {
+    const input1 = document.getElementById('input-1');
+    const input2 = document.getElementById('input-2');
+    const button = document.getElementById(`click-${id}`);
+    const displayResult = document.getElementById('display-result');
+    
+    button.addEventListener('click', () => {
+        const num1 = +input1.value;
+        const num2 = +input2.value;
+        const result = operation(num1, num2);
+        displayResult.textContent = result;
+    });
+}
+setupOperation('add', add);
+setupOperation('subtract', subtract);
+setupOperation('multiply', multiply);
+setupOperation('divide', divide);
